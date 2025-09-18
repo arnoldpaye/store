c8c3541dde5be5cb7654970be1b3438a31fec1a0
Arnold Paye <arnoldpayet@gmail.com>
2025-09-18 10:12:49 -0400

    feat: implement notification service

---
+++ src/main/java/com/arnex/store/EmailNotificationService.java
@@ -0,0 +1,11 @@
+package com.arnex.store;
+
+import org.springframework.stereotype.Service;
+
+@Service("email")
+public class EmailNotificationService implements NotificationService {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
---
+++ src/main/java/com/arnex/store/NotificationService.java
@@ -0,0 +1,5 @@
+package com.arnex.store;
+
+public interface NotificationService {
+    void send(String message);
+}
