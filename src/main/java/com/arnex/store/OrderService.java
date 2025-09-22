ee33d5cb4a90d757080364e8e4079e714920c58e
Arnold Paye <arnoldpayet@gmail.com>
2025-09-22 10:19:21 -0400

    feat: bean lifecycle hoooks

--- src/main/java/com/arnex/store/OrderService.java
+++ src/main/java/com/arnex/store/OrderService.java
@@ -1,11 +1,24 @@
 package com.arnex.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class OrderService {
     private PaymentService paymentService;
 
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
         System.out.println("OrderService created");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("OrderService PostConstruct");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("OrderService PreDestroy");
     }
 
     public void placeOrder() {
