593be1b71682d089083f83310363f7fca50b20af
Arnold Paye <arnoldpayet@gmail.com>
2025-09-24 09:47:39 -0400

    feat: add category and product entities

---
+++ src/main/java/com/arnex/store/entities/Category.java
@@ -0,0 +1,31 @@
+package com.arnex.store.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Byte id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products = new HashSet<>();
+}
