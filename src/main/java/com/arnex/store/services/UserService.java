ccc3e69b5203d8d0f04d3a62fa0c61512a9c99f9
Arnold Paye <arnoldpayet@gmail.com>
2025-09-25 08:54:34 -0400

    feat: add user service

---
+++ src/main/java/com/arnex/store/services/UserService.java
@@ -0,0 +1,30 @@
+package com.arnex.store.services;
+
+import org.springframework.stereotype.Service;
+
+import com.arnex.store.entities.User;
+import com.arnex.store.repositories.UserRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final EntityManager entityManager;
+
+    @Transactional
+    public void showEntityStates() {
+        var user =  User.builder().name("Arnex").email("arnex@email.com").password("password").build();
+
+        userRepository.save(user);
+
+        if (entityManager.contains(user)) {
+            System.out.println("Persistent");
+        } else {
+            System.out.println("Transient / Detached");
+        }
+    }
+}
